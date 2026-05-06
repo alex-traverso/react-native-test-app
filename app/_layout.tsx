ee69052d39eed25bd37e5fd302b1cfe6d2d07414
alex traverso <alextraverso6@gmail.com>
2026-05-06 10:52:02 -0300

    first commit

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,24 +1,45 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
-
-import { useColorScheme } from '@/hooks/use-color-scheme';
+import { useColorScheme } from "@/hooks/use-color-scheme";
+import { useThemeColor } from "@/hooks/use-theme-color";
+import ThemedText from "@/presentation/shared/ThemedText";
+import ThemedView from "@/presentation/shared/ThemedView";
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { StatusBar } from "expo-status-bar";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import "react-native-reanimated";
+import "../global.css";
 
 export const unstable_settings = {
-  anchor: '(tabs)',
+  anchor: "(tabs)",
 };
 
 export default function RootLayout() {
+  const backgroundColor = useThemeColor({}, "background");
+
   const colorScheme = useColorScheme();
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
+    <GestureHandlerRootView
+      style={{ backgroundColor: backgroundColor, flex: 1 }}
+    >
+      <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+        <ThemedView>
+          <ThemedText type="normal" className="">
+            Hola mundo x12
+          </ThemedText>
+        </ThemedView>
+        {/* <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+        <Stack.Screen
+          name="modal"
+          options={{ presentation: "modal", title: "Modal" }}
+        />
+      </Stack> */}
+        <StatusBar style="auto" />
+      </ThemeProvider>
+    </GestureHandlerRootView>
   );
 }
---
+++ presentation/shared/ThemedText.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { Text, TextProps } from "react-native";
+
+type TextType = "normal" | "h1" | "h2" | "semi-bold" | "bold" | "link";
+
+interface Props extends TextProps {
+  className?: string;
+  type?: TextType;
+}
+
+const ThemedText = ({
+  children,
+  className,
+  type = "normal",
+  ...rest
+}: Props) => {
+  return (
+    <Text
+      className={[
+        "text-light-text dark:text-dark-text",
+        type === "normal" ? "font-normal" : undefined,
+        type === "h1" ? "text-3xl" : undefined,
+        type === "h2" ? "text-xl" : undefined,
+        type === "semi-bold" ? "font-semibold" : undefined,
+        type === "bold" ? "font-bold" : undefined,
+        type === "link" ? "font-normal underline" : undefined,
+        className,
+      ].join(" ")}
+      {...rest}
+    >
+      {children}
+    </Text>
+  );
+};
+
+export default ThemedText;
